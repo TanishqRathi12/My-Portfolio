dcbcda66c77146e0922bd9f70deab166a2da2578
Tanishq Rathi <164673615+TanishqRathi12@users.noreply.github.com>
2024-05-17 17:52:26 +0530

    javaScript added

---
+++ index.js
@@ -0,0 +1,6 @@
+function paragraphchange(){
+    document.getElementById("demon2").innerHTML="Wohh! you changed the paragraph using JS";
+}
+const now = new Date();
+const datetime = now.toLocaleString();
+document.getElementById("demon1").innerHTML = datetime;
